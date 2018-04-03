e68808e6d4881846fdcdc664534c53b74d880a4e
Geoff Poole <geoff.poole@ementris.co.uk>
2018-04-03 14:48:05 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+
+    var log = `${now}: ${request.method} ${request.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    console.log(log);
+    next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs', {
+//         pageTitle: "Maintenance Page",
+//         message: "The site is currently being updated"
+//     });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+});
+
+// app.get('/', (request, response) => {
+//     // response.send('<h1>Hello Express!</h1>');
+//     response.send({
+//         name: 'Geoff',
+//         likes: [
+//             'Films',
+//             'Eating'
+//         ]
+//     });
+// });
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
